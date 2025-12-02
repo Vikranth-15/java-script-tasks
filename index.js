ae6b507b2264e135e603750598c1dad42e76f155
Vikranth <vikranthyadlapalli123@gmail.com>
2025-12-02 10:54:22 +0530

    season-switcher

---
+++ index.js
@@ -0,0 +1,32 @@
+let seasonSpringSmallImageElement = document.getElementById("seasonSmallImage");
+let seasonSpringMediumImageElement = document.getElementById("seasonMediumImage");
+
+function springBtnOne() {
+    seasonSpringSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    seasonSpringMediumImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+
+}
+
+function summerBtnOne() {
+    let seasonSummerSmallImageElement = document.getElementById("seasonSmallImage");
+    let seasonSummerMediumImageElement = document.getElementById("seasonMediumImage");
+
+    seasonSummerSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+    seasonSummerMediumImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+}
+
+function autumnBtnOne() {
+    let seasonAutumnSmallImageElement = document.getElementById("seasonSmallImage");
+    let seasonAutumnMediumImageElement = document.getElementById("seasonMediumImage");
+
+    seasonAutumnSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+    seasonAutumnMediumImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+}
+
+function winterBtnOne() {
+    let seasonWinterSmallImageElement = document.getElementById("seasonSmallImage");
+    let seasonWinterMediumImageElement = document.getElementById("seasonMediumImage");
+
+    seasonWinterSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+    seasonWinterMediumImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+}
